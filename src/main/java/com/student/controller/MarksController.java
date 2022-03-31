cdea8a2347cdca6254bb0029604ef603da29502e
Sagar Walke <sagarwalke7030@gmail.com>
2022-03-31 11:48:26 +0530

    base path added

--- src/main/java/com/student/controller/MarksController.java
+++ src/main/java/com/student/controller/MarksController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +25,8 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import java.util.Collections;
 import lombok.extern.slf4j.Slf4j;
 
-@RestController(value = "/marks")
+@RestController
+@RequestMapping("/marks")
 @SecurityRequirement(name = "studentapi")
 @Slf4j
 public class MarksController {
